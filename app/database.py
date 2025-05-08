7c5e4b43beb369b729559cf580c263bb5e73330f
inaa83 <ines.naamane@epitech.eu>
2025-05-08 23:53:55 +0200

    add dockerfile and update pipeline build and test

--- app/database.py
+++ app/database.py
@@ -6,7 +6,7 @@ load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL, echo=True)  # echo=True = log SQL dans console
+engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {})
 
 def init_db():
     SQLModel.metadata.create_all(engine)
