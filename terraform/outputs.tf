1e810f025f79e4f449123a29cdf004dfe1f5b5a1
inaa83 <ines.naamane@epitech.eu>
2025-05-11 15:52:39 +0200

    deploiement sur aws ec2 de l'app

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,8 @@
 output "rds_endpoint" {
   value = aws_db_instance.autonote_db.endpoint
 }
+
+output "ec2_public_ip" {
+  description = "Adresse IP publique de l'instance EC2 AutoNote"
+  value       = aws_instance.autonote_ec2.public_ip
+}
---
+++ terraform/user_data.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# mise à jour des paquets
+apt update -y
+apt upgrade -y
+
+# installation de docker et de git
+apt install -y docker.io git
+
+# je démarre docker
+systemctl start docker
+systemctl enable docker
+
+# install de docker compose
+DOCKER_CONFIG=/usr/libexec/docker/cli-plugins
+mkdir -p $DOCKER_CONFIG
+curl -SL https://github.com/docker/compose/releases/download/v2.27.0/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/docker-compose
+chmod +x $DOCKER_CONFIG/docker-compose
+
+# clone de mon dépot
+cd /home/ubuntu
+git clone https://github.com/nmnins/autonote-api.git
+cd autonote-api
+
+# 6. Lancer l'app (en arrière-plan)
+docker compose up -d
